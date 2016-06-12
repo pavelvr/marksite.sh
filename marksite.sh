8e90ff316ec9430390f94f93afb22cb41933709d
Pável Varela Rodríguez <neonskull@gmail.com>
2016-06-12 11:27:50 -0500

    bugs fixed; version v0.2
    - Fixed bug with paths on Windows: console's autocompletion uses \ (backslash), bash expects / (slash). Replacing \ with / in site-name and page-name
    - Fixed bug in first-time LESS compilation (related to modification dates in files .less and .css)
    - Updated version number

--- marksite.sh
+++ marksite.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-VERSION=0.1.2
+VERSION=0.2
 
 dump-template ()
 {
 	if [ ! $# -eq 1 ]; then echo "Usage: dump-template site-name";return;fi
-	TEMPLATE=$1/template.html
+
+	# fixing \ in path (for Windows console's autocompletion)
+	SITE=$(echo $1 | tr "\\\\" "/")
+
+	TEMPLATE=$SITE/template.html
 	if [ ! -f "$TEMPLATE" ]; then
-		echo Dumping default template to: $1
+		echo Dumping default template to: $SITE
 		pandoc -D html5 > "$TEMPLATE"
 	else
 		echo Template file already there...Doing nothing.
@@ -17,11 +21,16 @@ dump-template ()
 add-page ()
 {
 	if [ ! $# -eq 2 ];then echo "Usage: add-page site-name page-name";return;fi
-	PAGE=$1/content/$2.md
+
+	# fixing \ in path (for Windows console's autocompletion)
+	SITE=$(echo $1 | tr "\\\\" "/")
+	PAGE_NAME=$(echo $2 | tr "\\\\" "/")
+
+	PAGE=$SITE/content/$PAGE_NAME.md
 	if [ ! -f "$PAGE" ]; then
-		echo Creating new page: $2
+		echo Creating new page: $PAGE_NAME
 		mkdir -p "$(dirname "$PAGE")"
-		BASENAME=$(basename "$2")
+		BASENAME=$(basename "$PAGE_NAME")
 		CONTENT="---\r\n"
 		CONTENT=$CONTENT"pagetitle: "$BASENAME" page title\r\n"
 		CONTENT=$CONTENT"keywords:\r\n"
@@ -30,7 +39,7 @@ add-page ()
 		CONTENT=$CONTENT"Lorem ipsum...\r\n"
 		echo -ne $CONTENT > "$PAGE"
 	else
-		echo Page "$2" already exists...Doing nothing.
+		echo Page "$PAGE_NAME" already exists...Doing nothing.
 	fi
 }
 
@@ -38,9 +47,12 @@ init-menu ()
 {
 	if [ ! $# -eq 1 ];then echo "Usage: init-menu site-name";return;fi
 
-	if [ ! -f "$1/menu.md" ]; then
+	# fixing \ in path (for Windows console's autocompletion)
+	SITE=$(echo $1 | tr "\\\\" "/")
+
+	if [ ! -f "$SITE/menu.md" ]; then
 		echo Adding main menu to: $1
-		echo "- [Index](index.md)" > "$1/menu.md"
+		echo "- [Index](index.md)" > "$SITE/menu.md"
 	else
 		echo Main menu file already there...Doing nothing.
 	fi
@@ -49,10 +61,15 @@ init-menu ()
 init ()
 {
 	if [ ! $# -eq 1 ];then echo "Usage: init site-name";return;fi
-	echo Creating new site: $1
-	mkdir -p "$1/"{content,static/{css,js,images},www}
 
-	if [ ! -f "$1/metadata.yml" ]; then
+	# fixing \ in path (for Windows console's autocompletion)
+	SITE=$(echo $1 | tr "\\\\" "/")
+
+	echo Creating new site: $SITE
+
+	mkdir -p "$SITE/"{content,static/{css,js,images},www}
+
+	if [ ! -f "$SITE/metadata.yml" ]; then
 		METADATA="---\r\n"
 		METADATA=$METADATA"title-prefix:\r\n"
 		METADATA=$METADATA"title:\r\n"
@@ -60,32 +77,36 @@ init ()
 		METADATA=$METADATA"highlight: pygments\r\n"
 		METADATA=$METADATA"stylesheet: css/styles.css\r\n"
 		METADATA=$METADATA"---\r\n"
-		echo -ne $METADATA > "$1/metadata.yml"
+		echo -ne $METADATA > "$SITE/metadata.yml"
 	fi
 
-	CSS_FILE=$1/static/css/styles.css
+	CSS_FILE=$SITE/static/css/styles.css
 	test ! -f "$CSS_FILE" && echo -ne "/* your style rules here */\r\n" > "$CSS_FILE"
 
-	JS_FILE=$1/static/js/my.js
+	JS_FILE=$SITE/static/js/my.js
 	test ! -f "$JS_FILE" && echo -ne "/* your js code here */\r\n" > "$JS_FILE"
 
-	JS_INCLUDES=$1/js-includes.md
+	JS_INCLUDES=$SITE/js-includes.md
 	test ! -f "$JS_INCLUDES" && echo -ne "js/my.js" > "$JS_INCLUDES"
 
-	add-page $1 index
+	add-page $SITE index
 }
 
 build ()
 {
 	if [ ! $# -eq 1 ];then echo "Usage: build-site site-name";return;fi
-	echo -n "Building site: $1 ... "
 
-	cp -ru "$1/static/"* "$1/www"
+	# fixing \ in path (for Windows console's autocompletion)
+	SITE=$(echo $1 | tr "\\\\" "/")
+
+	echo -n "Building site: $SITE ... "
+
+	cp -ru "$SITE/static/"* "$SITE/www"
 
 	CSS_FILE="css/styles.css"
 	HIGHLIGHT=
-	if [ -f "$1/metadata.yml" ]; then
-		METADATA="$1/metadata.yml"
+	if [ -f "$SITE/metadata.yml" ]; then
+		METADATA="$SITE/metadata.yml"
 
 		HIGHLIGHT_STYLE=$(grep "highlight" "$METADATA"| sed -r "s/(\ |highlight:)//g")
 		test -z "$HIGHLIGHT_STYLE" -o "$HIGHLIGHT_STYLE" = "no" \
@@ -100,19 +121,22 @@ build ()
 			&& CSS_FILE="${CSS_FILE%.less}.css"
 	fi
 
-	test -f "$1/menu.md" && MENU="-V toc=\"{{MENU}}\"" || MENU=
-	test -f "$1/header.md" && HEADER="-A $1/.header.html" || HEADER=
-	test -f "$1/footer.md" && FOOTER="-A $1/.footer.html" || FOOTER=
-	test -f "$1/template.html" && TEMPLATE="--template=$1/template.html" || TEMPLATE=
+	test -f "$SITE/menu.md" && MENU="-V toc=\"{{MENU}}\"" || MENU=
+	test -f "$SITE/header.md" && HEADER="-A $SITE/.header.html" || HEADER=
+	test -f "$SITE/footer.md" && FOOTER="-A $SITE/.footer.html" || FOOTER=
+	test -f "$SITE/template.html" && TEMPLATE="--template=$SITE/template.html" || TEMPLATE=
 
-	test -f "$1/js-includes.md" && JS_INCLUDES="yes" || JS_INCLUDES="no"
+	test -f "$SITE/js-includes.md" && JS_INCLUDES="yes" || JS_INCLUDES="no"
 
 	PARAMS="-s -f markdown+yaml_metadata_block -t html5"
 
 	if [ $STYLESHEET_IS_LESS = "yes" ];then
-		less_file=$1/www/$STYLESHEET
+		less_file=$SITE/www/$STYLESHEET
 		css_file=${less_file%.less}.css
-		test ! -f "$css_file" && touch "$css_file"
+		# if css file doesn't exists, creating it with a modification date of
+		# 1 year ago, so, it doesn't matches less file modification date
+		# this fixes: css file not getting build the first time site builts
+		test ! -f "$css_file" && touch -d "1 year ago" "$css_file"
 		if [ ! "$(date +%s -r "$css_file")" = "$(date +%s -r "$less_file")" ];then
 			node bin/lessc/bin/lessc "$less_file" "$css_file"
 			touch -c -m -r "$less_file" "$css_file"
@@ -121,8 +145,8 @@ build ()
 
 	FILES=
 	DIRS=
-	for _file in $(find "$1/content" -type f -iname "*.md");do
-		# windows' version of find returns some \ in path, fixing it
+	for _file in $(find "$SITE/content" -type f -iname "*.md");do
+		# Windows' version of find returns some \ in path, fixing it
 		_file=$(echo $_file | tr "\\\\" "/")
 		FILES=$FILES$_file"\n"
 		DIRS=$DIRS"$(dirname $_file)\n"
@@ -132,39 +156,39 @@ build ()
 	# complex tests not working, so, doing it step by step
 	FORCE_REBUILD="no"
 	if [ -n "$MENU" ];then
-		if [ ! -f "$1/.menu.html" ];then
+		if [ ! -f "$SITE/.menu.html" ];then
 			FORCE_REBUILD="yes"
-		elif [ ! "$(date +%s -r "$1/.menu.html")" = "$(date +%s -r "$1/menu.md")" ];then
+		elif [ ! "$(date +%s -r "$SITE/.menu.html")" = "$(date +%s -r "$SITE/menu.md")" ];then
 			FORCE_REBUILD="yes"
 		fi
 	fi
 	if [ -n "$HEADER" ];then
-		if [ ! -f "$1/.header.html" ];then
+		if [ ! -f "$SITE/.header.html" ];then
 			FORCE_REBUILD="yes"
-		elif [ ! "$(date +%s -r "$1/.header.html")" = "$(date +%s -r "$1/header.md")" ];then
+		elif [ ! "$(date +%s -r "$SITE/.header.html")" = "$(date +%s -r "$SITE/header.md")" ];then
 			FORCE_REBUILD="yes"
 		fi
 	fi
 	if [ -n "$FOOTER" ];then
-		if [ ! -f "$1/.footer.html" ];then
+		if [ ! -f "$SITE/.footer.html" ];then
 			FORCE_REBUILD="yes"
-		elif [ ! "$(date +%s -r "$1/.footer.html")" = "$(date +%s -r "$1/footer.md")" ];then
+		elif [ ! "$(date +%s -r "$SITE/.footer.html")" = "$(date +%s -r "$SITE/footer.md")" ];then
 			FORCE_REBUILD="yes"
 		fi
 	fi
 	if [ "$JS_INCLUDES" = "yes" ];then
-		if [ ! -f "$1/.js-includes.html" ];then
+		if [ ! -f "$SITE/.js-includes.html" ];then
 			FORCE_REBUILD="yes"
-		elif [ ! "$(date +%s -r "$1/.js-includes.html")" = "$(date +%s -r "$1/js-includes.md")" ];then
+		elif [ ! "$(date +%s -r "$SITE/.js-includes.html")" = "$(date +%s -r "$SITE/js-includes.md")" ];then
 			FORCE_REBUILD="yes"
 		fi
 	fi
 
 	for _dir in $(echo -ne $DIRS | uniq); do
-		_out_dir=$1/www${_dir#"$1/content"}
+		_out_dir=$SITE/www${_dir#"$SITE/content"}
 		test ! -d "$_out_dir" && mkdir -p "$_out_dir"
 
-		_prefix=$(echo ${_out_dir#"$1/www"} | sed -e "s/[^\/]//g" -e "s/\//\.\.\//g")
+		_prefix=$(echo ${_out_dir#"$SITE/www"} | sed -e "s/[^\/]//g" -e "s/\//\.\.\//g")
 
 		# TODO: try to delay menu and js-includes regeneration unless it's
 		# actually needed (any file in $_dir will be (re)generated)
@@ -172,32 +196,32 @@ build ()
 
 		# preparing main menu for $_dir
 		if [ -n "$MENU" ]; then
-			pandoc -f markdown -t html5 "$1/menu.md" | sed -r "$href_sed_script" > "$1/.menu.html"
-			touch -c -m -r "$1/menu.md" "$1/.menu.html"
+			pandoc -f markdown -t html5 "$SITE/menu.md" | sed -r "$href_sed_script" > "$SITE/.menu.html"
+			touch -c -m -r "$SITE/menu.md" "$SITE/.menu.html"
 		fi
 
 		# preparing header for $_dir
 		if [ -n "$HEADER" ]; then
-			pandoc -f markdown -t html5 "$1/header.md" | sed -r "$href_sed_script" > "$1/.header.html"
-			touch -c -m -r "$1/header.md" "$1/.header.html"
+			pandoc -f markdown -t html5 "$SITE/header.md" | sed -r "$href_sed_script" > "$SITE/.header.html"
+			touch -c -m -r "$SITE/header.md" "$SITE/.header.html"
 		fi
 
 		# preparing footer for $_dir
 		if [ -n "$FOOTER" ]; then
-			pandoc -f markdown -t html5 "$1/footer.md" | sed -r "$href_sed_script" > "$1/.footer.html"
-			touch -c -m -r "$1/footer.md" "$1/.footer.html"
+			pandoc -f markdown -t html5 "$SITE/footer.md" | sed -r "$href_sed_script" > "$SITE/.footer.html"
+			touch -c -m -r "$SITE/footer.md" "$SITE/.footer.html"
 		fi
 
 		# preparing scripts block for $_dir
 		JS_INCLUDES_PARAM=
 		if [ "$JS_INCLUDES" = "yes" ]; then
 			JS=
-			for js_file in $(grep ".js" "$1/js-includes.md"); do
+			for js_file in $(grep ".js" "$SITE/js-includes.md"); do
 				JS=$JS"<script type=\"text/javascript\" src=\"$_prefix$js_file\"></script>\n"
 			done
-			echo -ne $JS > "$1/.js-includes.html"
-			touch -c -m -r "$1/js-includes.md" "$1/.js-includes.html"
-			JS_INCLUDES_PARAM="-A $1/.js-includes.html"
+			echo -ne $JS > "$SITE/.js-includes.html"
+			touch -c -m -r "$SITE/js-includes.md" "$SITE/.js-includes.html"
+			JS_INCLUDES_PARAM="-A $SITE/.js-includes.html"
 		fi
 
 		# processing files inside $_dir
@@ -221,7 +245,7 @@ build ()
 						-V date="$(date +%Y-%m-%d -r "$md_file")" \
 						$HEADER $HIGHLIGHT $TEMPLATE $FOOTER $JS_INCLUDES_PARAM \
 						"$md_file" "$METADATA" |\
-							sed -e "/{{MENU}}/r $1/.menu.html" -e '//d' |\
+							sed -e "/{{MENU}}/r $SITE/.menu.html" -e '//d' |\
 							sed -r "s/\.md\"/\.html\"/g" > "$html_file"
 					touch -c -m -r "$md_file" "$html_file"
 				fi
